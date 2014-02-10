c3e0549f3c1761af357705cdbaaf97ca02a4f56e
stltqhs <stltqhs2@gmail.com>
2014-02-10 18:20:06 +0800

    good

--- src/com/highdq/fucsdn/user/Manipulation.java
+++ src/com/highdq/fucsdn/user/Manipulation.java
@@ -1,5 +1,30 @@
 package com.highdq.fucsdn.user;
 
-public class Manipulation {
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
+/**
+ * csdn用户的操作。
+ * @author Yutsing Lee
+ *
+ */
+public class Manipulation {
+	/**
+	 * 请求登录的页面，里面包含参数替换
+	 */
+	private final static String REQUEST_LOGIN_PAGE = "https://passport.csdn.net/ajax/accounthandler.ashx?t=log&u=%s&p=%s&remember=0&f=http%3A%2F%2Fwww.csdn.net%2F&rand=%s";
+	
+	/**
+	 * csdn页面编码
+	 */
+	private final static String PAGE_ENCODING = "utf-8";
+	
+	public static CsdnConnection login(String username, String password) {
+		String requestLoginPage = null;
+		try {
+			String.format(REQUEST_LOGIN_PAGE, URLEncoder.encode(username, PAGE_ENCODING), URLEncoder.encode(password, PAGE_ENCODING), Math.random());
+		} catch (UnsupportedEncodingException e) {}
+		
+		return null;
+	}
 }
